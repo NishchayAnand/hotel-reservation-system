d6d5c8a5cfa154eff113c98be5eb315148bd0315
nishchayanand <nishchay.anand63@gmail.com>
2025-10-12 17:49:44 +0530

    developing the flow for hotel-details page

--- client/src/app/hotel-details/[id]/page.tsx
+++ client/src/app/hotel-details/[id]/page.tsx
@@ -9,12 +9,8 @@ import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/co
 import { Button } from "@/components/ui/button"
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
--- client/src/components/ui/hotel-card.tsx
+++ client/src/components/ui/hotel-card.tsx
@@ -50,7 +50,7 @@ export default function HotelCard(hotel: Hotel) {
       <div className="flex flex-col col-span-1 justify-end p-5">
         <p className="text-lg font-semibold">₹{avgRatePerNight}</p>
         <p className="text-xs text-gray-600 whitespace-nowrap mb-4">for {nights} night + Taxes</p>
-        <Link href={`/hotels/${id}`}>
+        <Link href={`/hotel-details/${id}`}>
           <Button className="w-full cursor-pointer font-semibold">View</Button>
         </Link>
       </div>
